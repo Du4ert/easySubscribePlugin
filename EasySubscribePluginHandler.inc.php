f783a53e904c5857468f0da4abe109f5ee39c3d8
Du4ert <du4ertb@gmail.com>
2021-08-09 16:06:08 +0300

    captcha secret > private

--- EasySubscribePluginHandler.inc.php
+++ EasySubscribePluginHandler.inc.php
@@ -49,7 +49,7 @@ class EasySubscribePluginHandler extends Handler {
 
         if($this->captchaEnabled)
         {
-            $secret = Config::getVar('captcha', 'recaptcha_secret_key');
+            $secret = Config::getVar('captcha', 'recaptcha_private_key');
             $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$recaptcha);
             $responseData = json_decode($verifyResponse);
             if(!$responseData->success) {

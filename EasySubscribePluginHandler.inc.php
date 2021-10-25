196cfcb377463fa1ea529987d69c39e65c9a428d
Du4ert <du4ertb@gmail.com>
2021-10-25 16:08:44 +0300

    refactor, + notification on registration

--- EasySubscribePluginHandler.inc.php
+++ EasySubscribePluginHandler.inc.php
@@ -36,7 +36,9 @@ class EasySubscribePluginHandler extends Handler {
         $recaptcha = $request->getUserVar('g-recaptcha-response');
         
         $easyEmailDao = DAORegistry::getDAO('EasyEmailDAO');
-        $message = '';
+        $message = [];
+        $messageString = '';
+        $status = 'success';
 
         if (!$csrfToken) {
             $templateMgr->assign([
@@ -54,7 +56,8 @@ class EasySubscribePluginHandler extends Handler {
             $secret = Config::getVar('captcha', 'recaptcha_private_key');
             $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$recaptcha);
             $responseData = json_decode($verifyResponse);
-            if(!$responseData->success) {
+            if(!!$responseData->success) {
+                $status = 'error';
                 if ($this->captchaEnabled) {
                     $publicKey = Config::getVar('captcha', 'recaptcha_public_key');
                     $reCaptchaHtml = '<div class="g-recaptcha" data-sitekey="' . $publicKey . '"></div>';
@@ -64,46 +67,39 @@ class EasySubscribePluginHandler extends Handler {
                     ));
                     $templateMgr->addJavaScript('recaptcha', 'https://www.recaptcha.net/recaptcha/api.js?hl=' . substr(AppLocale::getLocale(),0,2));
                 }
-                $message = __('plugins.generic.easySubscribe.form.captcha');
-                $templateMgr->assign([
-                    'status' => 'error',
-                    'message' => $message,
-                    'email' => $newEmail
-                ]);
-                return $templateMgr->display($this->plugin->getTemplateResource('subscribe.tpl'));
+                $message[] = __('plugins.generic.easySubscribe.form.captcha');
             }
          }
         
         if ($newEmail !== $confirmEmail) {
-            $message = __('plugins.generic.easySubscribe.page.register.confirm.error');
-            $templateMgr->assign([
-                'status' => 'error',
-                'message' => $message,
-                'email' => $newEmail
-            ]);
-            return $templateMgr->display($this->plugin->getTemplateResource('subscribe.tpl'));
+            $status = 'error';
+            $message[] = __('plugins.generic.easySubscribe.page.register.confirm.error');
         } 
 
-        if (!$easyEmailDao->getByEmail($this->contextId, $newEmail) && !!$newEmail) {
+        if (!!$easyEmailDao->getByEmail($this->contextId, $newEmail) || !$newEmail) {
+            $status = 'error';
+            $message[] = $newEmail ? __('plugins.generic.easySubscribe.form.error.exists') : __('plugins.generic.easySubscribe.form.error.empty');
+        } 
+
+        if ($status === 'success') {
             $easyEmail = $easyEmailDao->newDataObject();
             $easyEmail->setEmail((string) $newEmail, null);
             $easyEmail->setContextId((int) $this->contextId);
             $easyEmailDao->insertObject($easyEmail);
-            $message = __('plugins.generic.easySubscribe.form.success');
-            $templateMgr->assign([
-                'status' => 'success',
-                'message' => $message,
-                'email' => $newEmail
-            ]);
-        } 
-        else {
-        $message = $newEmail ? __('plugins.generic.easySubscribe.form.error.exists') : __('plugins.generic.easySubscribe.form.error.empty');
-        $templateMgr->assign([
-            'status' => 'error',
-            'message' => $message,
-            'email' => $newEmail
-		]);
+            $message[] = __('plugins.generic.easySubscribe.form.success');
+            $this->subscribeNotification($easyEmail);
         }
+
+        foreach ($message as $value) {
+            $messageString .=  "<p>$value</p>";
+        }
+
+        $templateMgr->assign([
+            'status' => $status,
+            'message' => $messageString,
+            'email' => $newEmail
+        ]);
+
         return $templateMgr->display($this->plugin->getTemplateResource('subscribe.tpl'));
     }
 
@@ -138,17 +134,49 @@ class EasySubscribePluginHandler extends Handler {
     }
 
     //! Функционал для тестирования. Нужно перенести в админ панель
-    // public function list($args, $request) {
-    //     $templateMgr = TemplateManager::getManager($request);
-    //     $easyEmailDao = DAORegistry::getDAO('EasyEmailDAO');
-    //     $emailsList = $easyEmailDao->getByContextId($this->contextId)->toArray();
+    public function list($args, $request) {
+        $templateMgr = TemplateManager::getManager($request);
+        $easyEmailDao = DAORegistry::getDAO('EasyEmailDAO');
+        $emailsList = $easyEmailDao->getByContextId($this->contextId)->toArray();
 
-    //     $templateMgr->assign([
-    //         'emailsList' => $emailsList,
-    //     ]);
+        $templateMgr->assign([
+            'emailsList' => $emailsList,
+        ]);
     
 
-    //     return $templateMgr->display($this->plugin->getTemplateResource('list.tpl'));
-    // }
+        return $templateMgr->display($this->plugin->getTemplateResource('list.tpl'));
+    }
+
+
+    protected function subscribeNotification($email) {
+        import('lib.pkp.classes.mail.Mail');
+        $request = Application::get()->getRequest();
+        $context = $request->getContext();
+
+		$fromEmail = $context->getData('contactEmail');
+		$fromName = $context->getData('contactName');
+		$headerTemplate= '<small>Это автоматическое уведомление с сайта ' . $context->getName('ru_RU') . '.</small>';
+		$footerTemplate = '<small>Чтобы отписаться от рассылки, перейдите по ссылке: <a href="URL">URL</a></small>';
+        $unsubscribeUrl = $request->getBaseUrl() . '/' . $context->getPath() . '/easysubscribe/unsubscribe';
+
+        $header = $headerTemplate;
+        $footer = str_replace('URL', $unsubscribeUrl . '?email=' . $email->getData('email') . '&id=' . $email->getId(), $footerTemplate);
+
+        $subject = 'Зарегестрирована подписка на уведомления';
+        $body = "<p>Вы получили это уведомление, потому что ваш почтовый ящик был зарегестрирован на сайте <a href=". $request->getBaseUrl() ." >".
+                $context->getName('ru_RU') . "</a></p><p>Если это были не вы, или вы передумали, перейдите по ссылке ниже.</p>";
+
+        $mail = new Mail();
+        $mail->setFrom($fromEmail, $fromName);
+        $mail->setRecipients([
+            [
+                'name' => '',
+                'email' => $email->getData('email'),
+            ],
+        ]);
+        $mail->setSubject($subject);
+        $mail->setBody($body . "<p>$header <br/> $footer</p>");
+        $mail->send();
+    }
 
 }
